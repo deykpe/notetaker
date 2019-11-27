0f07fb4ad74132f89e7e8e0a9f61a40241ae4ab3
David Pang <dpang2@uci.edu>
2019-11-26 19:41:44 -0800

    api and server

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const port = 8080
 const app = express();
+const apiRoutes = require("./routes/apiroutes");
+const htmlRoutes = require("./routes/htmlroutes");
 
  app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public")); 
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes); 
 
 
 
